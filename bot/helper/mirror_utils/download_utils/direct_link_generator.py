0ab531f9da68e8ab079755624d5e3b02b7ec4511
aenulrofik <aenulr97@gmail.com>
2024-04-01 03:11:14 +0800

    Add support for dc3 link terabox

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -862,8 +862,20 @@ def terabox(url):
                     if not details["title"]:
                         details["title"] = content["server_filename"]
                     folderPath = details["title"]
+                    
+                if content["dlink"]:
+                    link = content["dlink"]
+                    try:
+                        sublink = re.sub(r'd\.1024tera\.com', r'd3.terabox.app', link)
+                        c = async_to_sync(get_content_type, sublink)  
+                        if c is None or re.match(r"text/html|text/plain|text/json", c):
+                            link = link
+                        else:
+                            link = sublink                 
+                    except:
+                        raise DirectDownloadLinkException(f"ERROR: File tidak ditemukan")  
                 item = {
-                    "url": content["dlink"],
+                    "url": link,
                     "filename": content["server_filename"],
                     "path" : path.join(folderPath),
                 }
