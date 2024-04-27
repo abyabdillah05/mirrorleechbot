007b54d8e6852eb5d3e52e1dfb58e1014ad2575a
aenulrofik <aenulr97@gmail.com>
2024-04-28 00:49:34 +0800

    change media info to api

--- bot/modules/mediainfo.py
+++ bot/modules/mediainfo.py
@@ -1,114 +1,240 @@
-from aiohttp import ClientSession
-from re import search as re_search
-from shlex import split as ssplit
-from aiofiles import open as aiopen
-from aiofiles.os import remove as aioremove, path as aiopath, mkdir
-from os import path as ospath, getcwd
-
+import re
+import os
+import json
+import httpx
+import asyncio
+import requests
+import subprocess
+from async_timeout import timeout 
+from urllib.parse import unquote
 from pyrogram.handlers import MessageHandler 
 from pyrogram.filters import command
 
-from bot import LOGGER, bot, config_dict
+from bot import bot
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot.helper.telegram_helper.message_utils import editMessage, sendMessage
-from bot.helper.ext_utils.bot_utils import cmd_exec
-from bot.helper.ext_utils.telegraph_helper import telegraph
+from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage
 from bot.helper.telegram_helper.button_build import ButtonMaker
+from pyrogram.types import Message
+from bot.helper.ext_utils.mediainfo_utils import *
 
+async def ddl_mediainfo(message, url, isRaw):
+    """
+    Generates Mediainfo from a Direct Download Link.
+    """
 
-async def gen_mediainfo(message, link=None, media=None, mmsg=None):
-    temp_send = await sendMessage(message, '<i>⏳ Membuat media info...</i>')
-    try:
-        path = "Mediainfo/"
-        if not await aiopath.isdir(path):
-            await mkdir(path)
-        if link:
-            filename = re_search(".+/(.+)", link).group(1)
-            des_path = ospath.join(path, filename)
-            headers = {"user-agent":"Mozilla/5.0 (Linux; Android 12; 2201116PI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"}
-            async with ClientSession() as session:
-                async with session.get(link, headers=headers) as response:
-                    async with aiopen(des_path, "wb") as f:
-                        async for chunk in response.content.iter_chunked(10000000):
-                            await f.write(chunk)
-                            break
-        elif media:
-            des_path = ospath.join(path, media.file_name)
-            if media.file_size <= 50000000:
-                await mmsg.download(ospath.join(getcwd(), des_path))
-            else:
-                async for chunk in bot.stream_media(media, limit=5):
-                    async with aiopen(des_path, "ab") as f:
-                        await f.write(chunk)
-        stdout, _, _ = await cmd_exec(ssplit(f'mediainfo "{des_path}"'))
-        tc = f"<h4>ℹ️ {ospath.basename(des_path)}</h4><br><br>"
-        if len(stdout) != 0:
-            tc += parseinfo(stdout)
-    except Exception as e:
-        LOGGER.error(e)
-        await editMessage(temp_send, f"MediaInfo Stopped due to {str(e)}")
-    finally:
-        await aioremove(des_path)
-    link_id = (await telegraph.create_page(title='Pikabot MediaInfo', content=tc))["path"]
-    link_end = f"https://telegra.ph/{link_id}"
-    buttons = ButtonMaker()
-    buttons.ubutton("👁️ Lihat MediaInfo", f"{link_end}", "footer")
-    button = buttons.build_menu(1)
+    mess = await sendMessage(message,
+        "⌛️ Membuat mediainfo dari link anda, silahkan tunggu...")
     if message.from_user.username:
         uname = f'@{message.from_user.username}'
     else:
         uname = f'<code>{message.from_user.first_name}</code>'
-    await editMessage(temp_send, f"<b>Hai {uname}, Klik tombol dibawah untuk melihat MediaInfo dari file anda.</b>", button)
+    try:
+        filename = re.search(".+/(.+)", url).group(1)
+        if len(filename) > 60:
+            filename = filename[-60:]
 
+        rand_str = randstr()
+        download_path = f"downloads/{rand_str}_{filename}"
+        client = httpx.AsyncClient()  
+        headers = {
+            "user-agent": "Mozilla/5.0 (Linux; Android 12; 2201116PI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+        }
+        
+        async with timeout(15):
+            async with client.stream("GET", url, headers=headers) as response:
+                async for chunk in response.aiter_bytes(10000000):
+                    with open(download_path, "wb") as file:
+                        file.write(chunk)
+                    break
+                    
+        mediainfo = await async_subprocess(f"mediainfo {download_path}")
+        mediainfo_json = await async_subprocess(
+            f"mediainfo {download_path} --Output=JSON")
+        mediainfo_json = json.loads(mediainfo_json)
 
-section_dict = {'General': '🗒️', 'Video': '🎬', 'Audio': '🎵', 'Text': '📝', 'Menu': '📜'}
-def parseinfo(out):
-    tc = ''
-    trigger = False
-    for line in out.split('\n'):
-        for section, emoji in section_dict.items():
-            if line.startswith(section):
-                trigger = True
-                if not line.startswith('General'):
-                    tc += '</pre><br>'
-                tc += f"<h4>{emoji} {line.replace('Text', 'Subtitle')}</h4>"
-                break
-        if trigger:
-            tc += '<br><pre>'
-            trigger = False
-        else:
-            tc += line + '\n'
-    tc += '</pre><br>'
-    return tc
+        filesize = requests.head(url).headers.get("content-length")
+        lines = mediainfo.splitlines()
 
+        for i in range(len(lines)):
+            if "Complete name" in lines[i]:
+                lines[i] = re.sub(r": .+", ": " + unquote(filename), lines[i])
 
-async def mediainfo(_, message):
-    rply = message.reply_to_message
-    help_msg = "<b>File atau Media tidak ditemukan, silahkan balas ke media yang akan digenerate mediainfo !</b>"
-    if len(message.command) > 1 or rply and rply.text:
-        link = rply.text if rply else message.command[1]
-        return await gen_mediainfo(message, link)
-    elif rply:
-        if file := next(
-            (
-                i
-                for i in [
-                    rply.document,
-                    rply.video,
-                    rply.audio,
-                    rply.voice,
-                    rply.animation,
-                    rply.video_note,
-                ]
-                if i is not None
-            ),
-            None,
-        ):
-            return await gen_mediainfo(message, None, file, rply)
-        else:
-            return await sendMessage(message, help_msg)
+            elif "File size" in lines[i]:
+                lines[i] = re.sub(
+                    r": .+", ": " + get_readable_bytes(float(filesize)), lines[i])
+
+            elif (
+                "Overall bit rate" in lines[i]
+                and "Overall bit rate mode" not in lines[i]
+            ):
+                duration = float(mediainfo_json["media"]["track"][0]["Duration"])
+                bitrate = get_readable_bitrate(float(filesize) * 8 / (duration * 1000))
+                lines[i] = re.sub(r": .+", ": " + bitrate, lines[i])
+
+            elif "IsTruncated" in lines[i] or "FileExtension_Invalid" in lines[i]:
+                lines[i] = ""
+
+        with open(f"{download_path}.txt", "w") as f:
+            f.write("\n".join(lines))
+
+        if isRaw:
+            await message.reply_document(
+                f"{download_path}.txt", caption=f"<b>Nama File: `{filename}`")
+            os.remove(f"{download_path}.txt")
+            os.remove(f"{download_path}")
+
+        with open(f"{download_path}.txt", "r+") as file:
+            content = file.read()
+
+        output = mediainfo_paste(text=content, title=filename)
+        buttons = ButtonMaker()
+        buttons.ubutton("👁️ Lihat MediaInfo", f"{output}", "footer")
+        button = buttons.build_menu(1)
+        await sendMessage(message, f"<b>Hai {uname}, Klik tombol dibawah untuk melihat MediaInfo dari file anda.</b>", button)
+
+        os.remove(f"{download_path}.txt")
+        os.remove(f"{download_path}")
+
+    except asyncio.TimeoutError:
+        return await sendMessage(message,
+            f"Hai {uname}, Gagal membuat mediainfo dari link anda, link terlalu lama merespon :(" )
+               	
+    except Exception as error:
+        return await sendMessage(message,
+            f"Hai {uname}, Terjadi kesalahan saat mencoba membuat mediainfo dari link yang anda berikan.")
+    finally:
+        await deleteMessage(mess)
+
+async def telegram_mediainfo(client, message, isRaw):
+    """
+    Generates Mediainfo from a Telegram File.
+    """
+    mess = await sendMessage(message,
+        "⌛️ Membuat mediainfo dari media anda, silahkan tunggu...")
+    if message.from_user.username:
+        uname = f'@{message.from_user.username}'
     else:
-        return await sendMessage(message, help_msg)
+        uname = f'<code>{message.from_user.first_name}</code>'
+    try:
+        message = message.reply_to_message
+        if message.text:
+            return await sendMessage(message,
+                "Balas ke media yang mau anda generate bukan text, hadeh")
 
-bot.add_handler(MessageHandler(mediainfo, filters=command(BotCommands.MediaInfoCommand) & CustomFilters.authorized))
+        if message.media.value == "video":
+            media = message.video
+
+        elif message.media.value == "audio":
+            media = message.audio
+
+        elif message.media.value == "document":
+            media = message.document
+
+        elif message.media.value == "voice":
+            media = message.voice
+
+        else:
+            return await sendMessage(message,
+                "Media yang anda coba anda generate, belum disupport.")
+
+        filename = str(media.file_name)
+        size = media.file_size
+
+        rand_str = randstr()
+        download_path = f"downloads/{rand_str}_{filename}"
+
+        if int(size) <= 50000000:
+            await message.download(os.path.join(os.getcwd(), download_path))
+
+        else:
+            async for chunk in client.stream_media(message, limit=5):
+                with open(download_path, "ab") as f:
+                    f.write(chunk)
+
+        mediainfo = await async_subprocess(f"mediainfo '{download_path}'")
+        mediainfo_json = await async_subprocess(
+            f"mediainfo '{download_path}' --Output=JSON")
+        mediainfo_json = json.loads(mediainfo_json)
+
+        readable_size = get_readable_bytes(size)
+        lines = mediainfo.splitlines()
+        for i in range(len(lines)):
+            if "Complete name" in lines[i]:
+                lines[i] = re.sub(r": .+", ": " + unquote(filename), lines[i])
+
+            if "File size" in lines[i]:
+                lines[i] = re.sub(r": .+", ": " + readable_size, lines[i])
+
+            elif (
+                "Overall bit rate" in lines[i]
+                and "Overall bit rate mode" not in lines[i]
+            ):
+                duration = float(mediainfo_json["media"]["track"][0]["Duration"])
+                bitrate_kbps = (size * 8) / (duration * 1000)
+                bitrate = get_readable_bitrate(bitrate_kbps)
+                lines[i] = re.sub(r": .+", ": " + bitrate, lines[i])
+
+            elif "IsTruncated" in lines[i] or "FileExtension_Invalid" in lines[i]:
+                lines[i] = ""
+
+        remove_N(lines)
+        with open(f"{download_path}.txt", "w") as f:
+            f.write("\n".join(lines))
+
+        if isRaw:
+            await message.reply_document(
+                f"{download_path}.txt", caption=f"<b>Nama File:</b> `{filename}`")
+            os.remove(f"{download_path}.txt")
+            os.remove(f"{download_path}")
+
+        with open(f"{download_path}.txt", "r+") as file:
+            content = file.read()
+
+        output = mediainfo_paste(text=content, title=filename)
+        buttons = ButtonMaker()
+        buttons.ubutton("👁️ Lihat MediaInfo", f"{output}", "footer")
+        button = buttons.build_menu(1)
+        await sendMessage(message, f"<b>Hai {uname}, Klik tombol dibawah untuk melihat MediaInfo dari file anda.</b>", button)
+
+        os.remove(f"{download_path}.txt")
+        os.remove(download_path)
+
+    except Exception as error:
+        return await sendMessage(message,
+            f"Hai {uname}, Terjadi kesalahan saat membuat mediainfo dari media anda :(\n\n<code>{error}</code>")
+    finally:
+        await deleteMessage(mess)
+
+async def mediainfo(client, message: Message):
+    mediainfo_usage = f"<b>Generate media info dari media telegram atau dari direct link.</b> \n\nBalas pesan media dengan perintah <code>/{BotCommands.MediaInfoCommand[0]}</code> atau perintah disertai link."
+
+    if message.reply_to_message:
+        isRaw = False
+        if len(message.command) > 1:
+            user_input = message.text.split(None, 1)[1]
+            isRaw = bool(re.search(r"(-|--)r", user_input))
+        return await telegram_mediainfo(client, message, isRaw)
+
+    if len(message.command) < 2:
+        return await sendMessage(message, mediainfo_usage)
+
+    user_input = message.text.split(None, 1)[1]
+    isRaw = bool(re.search(r"(-|--)r", user_input))
+
+    if url_match := re.search(
+        r"https?://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])",
+        user_input,
+    ):
+        url = url_match.group(0)
+        return await ddl_mediainfo(message, url, isRaw)
+    return await sendMessage(message, "Link ini tidak disupport, silahkan periksa kembali link anda.")
+
+bot.add_handler(
+    MessageHandler(
+        mediainfo,
+        filters=command(
+            BotCommands.MediaInfoCommand)
+            & CustomFilters.authorized
+        )
+    )
