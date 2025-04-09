18b5d50b1575f7949d4a4d5075777a79a67d3d8e
Aby <abdillahyachya@gmail.com>
2025-04-09 17:03:28 +0700

    change main

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,7 +6,6 @@ from time import time
 from sys import executable
 from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
-from pyrogram.types import BotCommand, BotCommandScopeDefault
 from asyncio import create_subprocess_exec, gather
 from psutil import (
     disk_usage, 
@@ -499,42 +498,6 @@ help_string = f"""
 
 <b>NOTE :</b> Kirim perintah tanpa argument untuk melihat perintah secara detail!
 """
-
-bot.set_bot_commands([
-    BotCommand(f"{BotCommands.StartCommand}", "Mulai bot"),
-    BotCommand(f"{BotCommands.HelpCommand[0]}", "Tampilkan daftar perintah"),
-    BotCommand(f"{BotCommands.MirrorCommand[0]}", "Mirror ke Cloud"),
-    BotCommand(f"{BotCommands.QbMirrorCommand[0]}", "Mirror ke Cloud menggunakan qBittorrent"),
-    BotCommand(f"{BotCommands.YtdlCommand[0]}", "Mirror link yang disupport YT-DLP"),
-    BotCommand(f"{BotCommands.LeechCommand[0]}", "Leech ke Telegram"),
-    BotCommand(f"{BotCommands.QbLeechCommand[0]}", "Leech ke Telegram menggunakan qBittorrent"),
-    BotCommand(f"{BotCommands.YtdlLeechCommand[0]}", "Leech link yang disupport YT-DLP"),
-    BotCommand(f"{BotCommands.CloneCommand[0]}", "Menggandakan file/folder Google Drive"),
-    BotCommand(f"{BotCommands.CountCommand[0]}", "Menghitung file/folder Google Drive"),
-    BotCommand(f"{BotCommands.DeleteCommand[0]}", "Menghapus file/folder Google Drive"),
-    BotCommand(f"{BotCommands.UserSetCommand[0]}", "Pengaturan User"),
-    BotCommand(f"{BotCommands.BotSetCommand[0]}", "Pengaturan Bot"),
-    BotCommand(f"{BotCommands.RenameCommand[0]}", "Mengganti nama file/folder Google Drive"),
-    BotCommand(f"{BotCommands.ListCommand[0]}", "Mencari file/folder di Google Drive"),
-    BotCommand(f"{BotCommands.SearchCommand[0]}", "Mencari torrent menggunakan API"),
-    BotCommand(f"{BotCommands.GenTokenCommand}", "Authorize Google Drive"),
-    BotCommand(f"{BotCommands.GetTokenCommand}", "Get Google Drive Token"),
-    BotCommand(f"{BotCommands.StatusCommand[0]}", "Menampilkan status dari semua tugas yang sedang berjalan"),
-    BotCommand(f"{BotCommands.StatsCommand[0]}", "Menampilan statistik dari mesin bot"),
-    BotCommand(f"{BotCommands.PingCommand[0]}", "Mengetes respon bot"),
-    BotCommand(f"{BotCommands.CekQuotaCommand}", "Cek Quota Mirror atau Leech"),
-    BotCommand(f"{BotCommands.Upload_gofileCommand[0]}", "Upload ke Gofile"),
-    BotCommand(f"{BotCommands.Upload_buzzCommand[0]}", "Upload ke BuzzHeavier"),
-    BotCommand(f"{BotCommands.Upload_pixelCommand[0]}", "Upload ke PixelDrain"),
-    BotCommand(f"{BotCommands.GallerydlCommand[0]}", "Download dengan Gallery-DL"),
-    BotCommand(f"{BotCommands.Yt_searchCommand[0]}", "Cari dan download Video Youtube"),
-    BotCommand(f"{BotCommands.TiktokCommand[0]}", "Cari video random di Tiktok"),
-    BotCommand(f"{BotCommands.AsupanCommand[0]}", "Dapatkan asupan random"),
-    BotCommand(f"{BotCommands.AnimekCommand[0]}", "Dapatkan waifu random"),
-    BotCommand(f"{BotCommands.MediaInfoCommand[0]}", "Cek mediainfo dari file"),
-    BotCommand(f"{BotCommands.DirectCommand[0]}", "Bypass Shortlink"),
-    BotCommand(f"{BotCommands.DonateCommand}", "Donasi untuk bot ini"),
-], scope=BotCommandScopeDefault())
 
 async def bot_help(client, message):
     buttons = ButtonMaker()
