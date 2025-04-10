2d204cb8f3171a49b545a9c5b907631902789256
Aby <abdillahyachya@gmail.com>
2025-04-10 13:16:01 +0700

    Fix import paths for common utilities in status_utils and mega_download_status

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -7,8 +7,8 @@ from psutil import (
     virtual_memory
 )
 
-from .helper.ext_utils.common_utils import (get_readable_file_size,
-                                            get_readable_time)
+from bot.helper.ext_utils.common_utils import (get_readable_file_size,
+                                             get_readable_time)
 from bot import task_dict, task_dict_lock, botStartTime, config_dict, LOGGER, bot
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.button_build import ButtonMaker
